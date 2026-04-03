c68c402a33276525ca5f4c2664edc7d52999198c
vansh-cloud-00 <vansh.khatri855@gmail.com>
2026-04-03 13:18:18 +0530

    BinarySearch.java

---
+++ FirstandLastpositionofSortedArray.java
@@ -0,0 +1,38 @@
+
+  class Solution {
+    public int[] find(int[] arr, int x) {
+        int first = -1, last = -1;
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == x) {
+                first = mid;
+                high = mid - 1;
+            } else if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        low = 0;
+        high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == x) {
+                last = mid;
+                low = mid + 1;
+            } else if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return new int[]{first, last};
+    }
+}
